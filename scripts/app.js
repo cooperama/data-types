b9415f25559d06e8e10317989914178f78decf5d
cooperama <coope133@gmail.com>
2020-09-16 16:12:39 -0700

    Commit 4 - object-ception

--- scripts/app.js
+++ scripts/app.js
@@ -171,35 +171,36 @@ console.log(crazyObject.larry.characters[1].name);
 
 // // ---
 
-// console.log('--------------------------------------');
-// console.log('4. Object-ception');
-// console.log('--------------------------------------');
+console.log('--------------------------------------');
+console.log('4. Object-ception');
+console.log('--------------------------------------');
 // // ### 4. Object-ception
 
 // // With the following object:
 
 // // ```js
-// const inception = {
-//    reality: {
-//        dreamLayer1: {
-//            dreamLayer2: {
-//                dreamLayer3: {
-//                    dreamLayer4: {
-//                        dreamLayer5: {
-//                            dreamLayer6: {
-//                                limbo: "Joseph Gordon Levitt"
-//                           }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-// }
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
 // ```
 
 // Change the value of `limbo` to `null`.
-
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
 // ---
 
 // &#x1F534; **Commit:** "object-ception"
