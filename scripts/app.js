ffd0e43a1cc4df9ede4a66207aaa2ca543d50fed
cooperama <coope133@gmail.com>
2020-09-16 16:10:28 -0700

    Commit 3 - crazyObject

--- scripts/app.js
+++ scripts/app.js
@@ -115,61 +115,70 @@ console.log('--------------------------------------');
 // ### 3. Crazy Object!
 
 // ```javascript
-// const crazyObject = {
-//   taco: [
-//     {
-//       meat: 'steak',
-//       cheese: ['panela', 'queso', 'chihuahua']
-//     },
-//     {
-//       meat: 'chicken',
-//       salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-//     },
-//   ],
-//   larry: {
-//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-//     characters: [
-//       {
-//         name: "Jeff",
-//         occupation: "manager"
-//       },
-//       {
-//         name: "funkhauser",
-//         occupation: "tv dude"
-//       },
-//       {
-//         name: "susie",
-//         occupation: "jeffs wife",
-//         favourtieHobby: "Swearing at Larry and Jeff"
-//       },
-//     ]
-//   }
-// }
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
+}
 // ```
 
-// Use crazyObject to log the following.
+// // Use crazyObject to log the following.
 
-//   * "omg my mouth is burning"
-//   * "Pretty pretty prettayyyyy good"
-//   * "Swearing at Larry and Jeff"
-//   * "Chicken Teriyaki Boyyyyyy"
-//   * The object the contains the name `funkhauser`
+// //   * "omg my mouth is burning"
+console.log(crazyObject.taco[1].salsa[5]);
 
-// ---
+// //   * "Pretty pretty prettayyyyy good"
+console.log(crazyObject.larry.quotes[0]);
 
-// &#x1F534; **Commit:** "crazyObject"
+// //   * "Swearing at Larry and Jeff"
+console.log(crazyObject.larry.characters[2].favourtieHobby);
 
-// ---
+// //   * "Chicken Teriyaki Boyyyyyy"
+console.log(crazyObject.larry.nicknames[1]);
 
-console.log('--------------------------------------');
-console.log('4. Object-ception');
-console.log('--------------------------------------');
-// ### 4. Object-ception
+// //   * The object the contains the name `funkhauser`
+console.log(crazyObject.larry.characters[1].name);
 
-// With the following object:
+// // ---
 
-// ```js
+// // &#x1F534; **Commit:** "crazyObject"
+
+// // ---
+
+// console.log('--------------------------------------');
+// console.log('4. Object-ception');
+// console.log('--------------------------------------');
+// // ### 4. Object-ception
+
+// // With the following object:
+
+// // ```js
 // const inception = {
 //    reality: {
 //        dreamLayer1: {
